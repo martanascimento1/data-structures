2aeec2e1c7563395e6a300db7820838e09ed4080
martanascimento1 <mmns5@aluno.ifal.edu.br>
2023-12-04 19:31:32 -0300

    ;

--- binary search/q1.c
+++ binary search/q1.c
@@ -1,36 +1,38 @@
 #include<stdio.h>
 #include <stdlib.h>
 
-int buscaBinaria(int *vetor, int valor, int inicio, int fim) {
-    if (inicio <= fim) {
-        int meio = (inicio + fim) / 2;
-        if (vetor[meio] == valor) {
-            return meio;
-        } else if (vetor[meio] < valor) {
-            return buscaBinaria(vetor, valor, meio + 1, fim);
-        } else {
-            return buscaBinaria(vetor, valor, inicio, meio - 1);
+int binarySearch(int *v, int size, int valor) {
+    int begin = 0;
+    int end = size - 1;
+    int middle;
+     while (begin <= end ) {
+        middle = (begin + end) / 2;
+        if (v[middle] < valor) {
+            begin = middle + 1;
         }
-    }else {
-    	return -1;
-    }
+        else if (v[middle] > valor) {
+            end = middle - 1;
+        }
+        else {
+            return middle;
+        }
+     }
+     return -1;
 }
-
-
 int main ()
 {
 
- int vetor[10]={1,3,2,8,3,4,6,5,9,10};
- int resultado = 0;
- int valor = 0;
+ int array[16]={1,3,2,8,3,4,6,5,9,10, 11, 12, 13, 14, 15, 16};
+ int result = 0;
+ int v = 0;
 
- printf("Informe o valor a ser encontrado:\n");
- scanf("%d",&valor);
- resultado = buscaBinaria(vetor, valor, 0, 10);
- if(resultado == -1){
- 	printf("\nValor nao encontrado!\n");
+ printf("Enter the value you want to search:\n");
+ scanf("%d",&v);
+ result = binarySearch(array, 16, v );
+ if(result == -1){
+ 	printf("value not found\n");
  }else{
- 	printf("\nValor encontrado!\n");
+ 	printf("value found\n");
  }
  return 0;
 
