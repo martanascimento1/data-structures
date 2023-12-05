b37b5e4dd66b20049fa026a2e20e458d6f286e6b
martanascimento1 <mmns5@aluno.ifal.edu.br>
2023-12-04 23:03:52 -0300

    ;

---
+++ dynamic memory allocation/aloc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	printf("\nSize of char: %d", sizeof(char));
+	printf("\nSize of int: %d", sizeof(int));
+	printf("\nSize of float: %d", sizeof(float));
+	printf("\nSize of double: %d\n", sizeof(double));
+	
+}
