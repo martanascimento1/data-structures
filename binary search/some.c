6ff9d9a5c9538e5e1dce2325d29a122df8a4dc4e
Marta Mirely Nascimento dos Santos <103062784+martanascimento1@users.noreply.github.com>
2024-01-11 23:20:29 -0300

    ;

---
+++ binary search/some.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+
+    int a, b, some;
+    printf(digite os valores de a e b:\n);
+    scanf("%d%d", &a, &b);;
+    some = a + b
+    printf("%d\n", some);
+    return 0;
+}
