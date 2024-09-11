5abb3d4c4953b10c35e3c9e90b7955d1b6866729
marta.nascimento <mrtmirely@gmail.com>
2024-09-11 16:14:21 -0300

    ;

---
+++ stack/huxley-exercises/shinobu.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//Armazena o primeiro item da fila
+typedef struct _stack STACK;
+
+//Armazena o conteudo do no e um ponteiro para o proximo no da fila
+typedef struct _node NODE;
+
+struct _stack
+{
+    NODE* head;
+};
+struct _node
+{
+    int element;
+    NODE* next;
+};
+
+//Cria uma stack com o a head NULL
+STACK* Create_stack() {
+     STACK *new_stack = (STACK*) calloc(1, sizeof(STACK));
+    
+    new_stack->head = NULL;
+    
+    return new_stack;
+}
+
+//Recebe um elemento e cria e retorna um novo node
+// com o elemento passado
+NODE* create_node(int element) {
+     NODE *new_node = calloc(1, sizeof(NODE));
+
+    new_node->element = element;
+
+
+    return new_node;
+}
+
+//Verifica se a pilha esta vazia
+int IS_EMPTY(STACK* stack) {
+    return stack->head == -1;
+}
+
+//Recebe uma pilha e Retorna o elemento que esta no topo da fila
+int POP(STACK* stack) {
+      if(IS_EMPTY(stack))
+    {
+        puts("Underflow");
+        return -1;
+    }
+    
+    NODE *aux = stack->head;
+
+    stack->head = aux->next;
+
+    return aux->element;
+}
+
+//Recebe uma pilha e um inteiro e retorna a nova pilha
+//Adiciona um novo node no topo da pilha
+void PUSH(STACK* stack, int element) {
+     NODE *new_node = create_node(element);
+    
+    new_node->next = stack->head;
+
+    stack->head = new_node;
+
+    return;
+}
+
+//Recebe a pilha e a operacao a ser feita
+//faz a operacao da calculadora
+void result(STACK* stack, char operation) {
+    int v1 = POP(stack);
+    int v2 = POP(stack);
+    int resultado;
+
+    if (operation =='*') {
+        resultado = v2 * v1;
+    }
+    if (operation == '+') {
+        resultado = v2 + v1;
+    }
+    if (operation == '/') {
+        resultado = v2 / v1;
+    }
+    if (operation == '-') {
+        resultado = v2 - v1;
+    }
+
+    PUSH(stack, resultado);
+}
+
+//Recebe uma pilha vazia e quantas strings serao lidas
+//Le as n strings que vao seguir e resolve as expressoes
+void Calculadora(STACK* calculadora, int size) {
+
+    for(int i = 0; i < size; i++)
+    {
+        char operacao[50];
+
+        scanf("%s", operacao);
+
+        if(operacao[0] == '+' || operacao[0] == '-' || operacao[0] == '/' || operacao[0] == '*')
+        {
+            result(calculadora,operacao[0]);
+        }
+        else
+        {
+            int item = atoi(operacao);
+            PUSH(calculadora,item);
+        }
+} }
+
+int main()
+{
+    STACK* calculadora = Create_stack();
+    int k;
+    scanf("%d", &k);
+    Calculadora(calculadora, k);
+    printf("Coordenada 1: %d\n", POP(calculadora));
+    scanf("%d", &k);
+    Calculadora(calculadora, k);
+    printf("Coordenada 2: %d\n", POP(calculadora));
+
+}
+//Insira o código aqui
--- stack/huxley-exercises/two_stacks.c
+++ stack/huxley-exercises/two_stacks.c
@@ -9,60 +9,62 @@ typedef struct pilha_dupla {
 } TPilhaDupla;
 
 void create(TPilhaDupla *pd) {
-	TPilhaDupla *p = (TPilhaDupla*)malloc(sizeof(TPilhaDupla));
     pd->topo1 = -1;
     pd->topo2 = TAM;
 }
 
 void destroy(TPilhaDupla *pd) {
-	free(pd);
+	pd->topo1 = -1;
+    pd->topo2 = TAM;
 }
 
 int isfull(TPilhaDupla *pd) {
-    return pd->topo1 == pd->topo2 -1;
+    return pd->topo1 +1 == pd->topo2;
 }
 
 int isempty(TPilhaDupla *pd, int topo) {
-	if (topo == 1) {
-        return pd->topo1 == -1;
-    }
-    else if (topo == 2) {
-        return pd->topo2 == TAM;
-    }
-    return -1;
+	return (topo ==1 && pd->topo1 == -1) || (topo == 2 && pd->topo2 == TAM);
     }
 
 
 void push(TPilhaDupla *pd, ITEM x, int topo) {
-	if (pd->topo1 < pd->topo2 -1) {
-        if (topo == 1) {
-            pd->topo1++;
-            pd->vet[pd->topo1] = x;
-        }
-        else if (topo == 2){
-            pd->topo2--;
-            pd->vet[pd->topo2] = x;
-        }
+	if(isfull(pd))
+    {
+       return;
     }
+
+    if(topo == 1)
+    {
+        pd->topo1++;
+        pd->vet[pd->topo1] = x;
+    }
+    else
+    {
+        pd->topo2--;
+        pd->vet[pd->topo2] = x;
+    }
+
 }
 
 ITEM pop(TPilhaDupla *pd, int topo) {
-	if (topo == 1) {
-        if (pd->topo1 >= 0) {
-            int x = pd->vet[pd->topo1];
-            pd->topo1--;
-            return x;
-        }
- }
-        else if (topo == 2 ) {
-            if (pd->topo2 < TAM) {
-            int x = pd->vet[pd->topo2];
-            pd->topo2++;
-            return x;
-            }
-            
-        }  
-        return -1;
+	if(isempty(pd, topo))
+    {
+       return;
+    }
+
+    char x;
+    if(topo == 1)
+    {
+        x = pd->vet[pd->topo1];
+        pd->topo1--;
+    }
+    if(topo == 2)
+    {
+        x = pd->vet[pd->topo2];
+        pd->topo2++;
+    }
+
+    return x;
     }
 
 ITEM top(TPilhaDupla *pd, int topo) {
